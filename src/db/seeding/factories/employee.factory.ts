e9d9b31aed11fe1f2e8aea3015318f331911c3ed
JosepSalvat <josep.salvat@arbolus.com>
2023-06-16 14:00:51 +0200

    FIX

--- src/db/seeding/factories/employee.factory.ts
+++ src/db/seeding/factories/employee.factory.ts
@@ -10,7 +10,9 @@ define(Employee, (faker) => {
   user.email = faker.internet.email(firstName, lastName);
   user.quote = faker.lorem.sentence(2000);
   user.salary = faker.random.number({ min: 25000, max: 150000 });
-  user.imageUrl = 'https://picsum.photos/200';
+  user.imageUrl = `https://loremflickr.com/320/240?random=${faker.random.number(
+    { min: 1, max: 100 },
+  )}`;
   user.gender = faker.random.arrayElement([
     EmployeeGender.MALE,
     EmployeeGender.FEMALE,
--- src/db/seeding/seeds/initialSeed.ts
+++ src/db/seeding/seeds/initialSeed.ts
@@ -4,6 +4,6 @@ import { Employee } from 'src/db/employees.entity';
 
 export default class InitialDatabaseSeed implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
-    await factory(Employee)().createMany(1500);
+    await factory(Employee)().createMany(1000);
   }
 }
